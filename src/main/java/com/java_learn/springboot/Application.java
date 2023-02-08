3ad388d9c67e7ec14be90e6a55813ae7ab5a00eb
Prince-Davy <princedavy.nkakou@gmail.com>
2023-02-08 15:01:04 +0100

    Datasource Configuration - Install PostgresSQL JDBC Driver (Maven) - Spring Data JPA (Maven) - Configure application.yml file

--- src/main/java/com/java_learn/springboot/Application.java
+++ src/main/java/com/java_learn/springboot/Application.java
@@ -2,10 +2,7 @@ package com.java_learn.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,25 +10,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @GetMapping("/")
-    public GreetResponse greet() {
-        return new GreetResponse(
-                "Hello",
-                List.of("Java", "JavaScript", "C#"),
-                new Person("John",
-                        19,
-                        350.0));
-    }
-
-    record GreetResponse(
-            String greeting,
-            List<String> favProgrammingLangages,
-            Person person) {
-    }
-
-    record Person(String name, int age, double availableCash) {
-
     }
 }
