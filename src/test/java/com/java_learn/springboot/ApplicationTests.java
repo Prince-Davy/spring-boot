d970b2e12d81001784eeee19a0cb3cedf112512f
Prince-Davy <princedavy.nkakou@gmail.com>
2023-02-13 11:38:54 +0100

    Databases and Spring Data JPA Create CustomerRepository Configure yml file Interface to interact with Customer database

--- src/test/java/com/java_learn/springboot/ApplicationTests.java
+++ src/test/java/com/java_learn/springboot/ApplicationTests.java
@@ -1,9 +1,7 @@
 package com.java_learn.springboot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
 
     @Test
